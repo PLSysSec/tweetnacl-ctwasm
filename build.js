764ca34ff98c0b9f0d1b62777f1d6bc61d5aedc2
scauligi <scauligi@eng.ucsd.edu>
2018-10-16 10:49:56 +0200

    updated build.js via john

--- build.js
+++ build.js
@@ -1,10 +1,9 @@
-const wast2wasm = require('wast2wasm');
 const fs = require('fs');
 const path = require('path');
 
 const watSourceDirectory = './src/wat';
 const sourceDirectory = './src';
-const wasmOuputDirectory = './dist';
+const wasmOutputDirectory = './dist';
 
 const naclWasmSourceSearchString = 'const wasmCode = \'\';'
 
@@ -16,23 +15,33 @@ const codeArray = filesToAssemble.map(fileName => {
 	return fs.readFileSync(path.join(watSourceDirectory, fileName)).toString('utf8');
 });
 
-const completeModule = '(module (import "js" "mem" (memory 1))' + 
+const completeModule = '(module (import "js" "mem" (memory secret 1))' + 
 	codeArray.join('\n') + ')';
 
+const {promisify} = require('util');
+const {exec} = require('child_process');
+
+const wat = path.join(wasmOutputDirectory, 'build.wat');
+const wasm = path.join(wasmOutputDirectory, 'build.wasm');
+
+function wast2wasm(m, _) {
+	fs.writeFileSync(wat, m);
+	return promisify(exec)(`${process.argv[2]} -d -i ${wat} -o ${wasm}`,{});
+		
+}
 wast2wasm(completeModule, true)
 	.then(output => {
-		fs.writeFileSync(path.join(wasmOuputDirectory, 'build.wasm'), output.buffer);
-		fs.writeFileSync(path.join(wasmOuputDirectory, 'build.log'), output.log);
-		return new Buffer(output.buffer).toString('base64');
+		return fs.readFileSync(wasm).toString('base64');
 	})
 	.then(output => {
 		let chunkedOutput = output.match(/.{1,100}/g).map(chunk => JSON.stringify(chunk)).join(' +\n\t\t');
 		let naclWasmSource = fs.readFileSync(path.join(sourceDirectory, 'nacl-wasm.js')).toString('utf8');
 		naclWasmSource = naclWasmSource.replace(naclWasmSourceSearchString, 'const wasmCode =\n\t\t' + chunkedOutput);
 		
-		if (!fs.existsSync(wasmOuputDirectory)) {
-			fs.mkdirSync(wasmOuputDirectory);
+		if (!fs.existsSync(wasmOutputDirectory)) {
+			fs.mkdirSync(wasmOutputDirectory);
 		}
 
-		fs.writeFileSync(path.join(wasmOuputDirectory, 'nacl-wasm.js'), naclWasmSource);
+		fs.writeFileSync(path.join(wasmOutputDirectory, 'nacl-wasm.js'), naclWasmSource);
 	});
+
